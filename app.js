34dec3feb25e808729304d4b54b6152c468e1a39
Chris-george-anil <66996004+Chris-george-anil@users.noreply.github.com>
2020-09-14 14:14:15 +0530

    Update app.js
    Authentication

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const express=require('express');
-const bodyparser=require('body-parser');
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate=require("mongoose-findorcreate");
+const bcrypt=require("bcrypt");
+const saltrounds=10;
+
+mongoose.connect('mongodb://localhost:27017/VirQue', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set("useCreateIndex", true);
+
+const UserSchema = new mongoose.Schema({
+  Email: String,
+  Name:String,
+  Aadhar:String,
+  PhoneNum:Number,
+  Password: String,
+});
+
+const User = new mongoose.model("User", UserSchema);
 
 const app=express();
 app.set('view engine','ejs');
 app.use(express.static("public"));
-
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 app.get("/",function(req,res){
-  res.render("home");
+  res.render("index");
 });
 
 app.get("/about",function(req,res){
@@ -15,11 +42,51 @@ app.get("/about",function(req,res){
 app.get("/contact",function(req,res){
   res.render("contact");
 });
+app.get("/signup",function(req,res){
+  res.render("signup");
+});
 app.get("/login",function(req,res){
   res.render("login");
 });
-app.get("/signup",function(req,res){
-  res.render("signup");
+app.post("/login",function(req,res){
+  const num=req.body.num;
+  const password=req.body.password;
+  User.findOne({PhoneNum:num},function(err,found){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(found){
+        bcrypt.compare(password, found.Password, function(err, result) {
+    if (result === true) {
+    res.render("home");
+  }else{
+    console.log("Wrong Password");
+  }
+});
+}else{
+  console.log("Not Found");
+}
+    }
+  })
+});
+app.post("/signup",function(req,res){
+  const pass=req.body.password;
+    bcrypt.hash(pass,saltrounds,function(err,hash){
+      const newUser=new User({
+        Email: req.body.email,
+        Name:req.body.name,
+        Aadhar:req.body.aadhar,
+        PhoneNum:req.body.phone,
+        Password: hash,
+      });
+      newUser.save(function(err){
+        if(err)
+        console.log(err);
+        else
+        res.render("home")
+      })
+    });
 });
 app.listen(4500,function(err){
   if(!err){
